dcb253a2ba8dea2b1c26f77645d14ed73708cd01
Kyle <kzunino@gmail.com>
2020-11-09 18:33:38 -0800

    added red to autocomplete delete indicator button

--- src/components/NewTrip.js
+++ src/components/NewTrip.js
@@ -91,6 +91,9 @@ const useStyles = makeStyles((theme) => ({
     '& .MuiSelect-icon': {
       color: theme.palette.primary.main,
     },
+    '& .MuiAutocomplete-clearIndicator': {
+      color: '#D61A3C',
+    },
     //  '& .MuiInput-input':{
     //   color: theme.palette.secondary.main
     //  },
